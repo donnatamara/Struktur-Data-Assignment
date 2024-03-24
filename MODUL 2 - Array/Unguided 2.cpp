663d64775df369030aa1f3b5bd6e9718a8d4b510
Darussalaam Nur Rasyidu <donnatamara820@gmail.com>
2024-03-24 20:53:39 +0700

    Update Unguided 2

--- MODUL 2 - Array/Unguided 2.cpp
+++ MODUL 2 - Array/Unguided 2.cpp
@@ -6,25 +6,31 @@ int main() {
 
   // Minta pengguna memasukkan dimensi
   cout << "Masukkan jumlah elemen untuk setiap dimensi:\n";
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 3; ++i) 
+  {
     cout << "Dimensi " << i + 1 << ": ";
     cin >> dimensi[i];
   }
 
   // Alokasikan memori secara dinamis untuk array 3 dimensi
   int*** arr = new int**[dimensi[0]];
-  for (int i = 0; i < dimensi[0]; ++i) {
+  for (int i = 0; i < dimensi[0]; ++i) 
+  {
     arr[i] = new int*[dimensi[1]];
-    for (int j = 0; j < dimensi[1]; ++j) {
+    for (int j = 0; j < dimensi[1]; ++j) 
+    {
       arr[i][j] = new int[dimensi[2]];
     }
   }
 
   // Masukkan elemen
   cout << "\nMasukkan elemen array:\n";
-  for (int i = 0; i < dimensi[0]; ++i) {
-    for (int j = 0; j < dimensi[1]; ++j) {
-      for (int k = 0; k < dimensi[2]; ++k) {
+  for (int i = 0; i < dimensi[0]; ++i) 
+  {
+    for (int j = 0; j < dimensi[1]; ++j) 
+    {
+      for (int k = 0; k < dimensi[2]; ++k) 
+      {
         cout << "Array[" << i << "][" << j << "][" << k << "] = ";
         cin >> arr[i][j][k];
       }
@@ -33,9 +39,12 @@ int main() {
 
   // Tampilkan elemen array
   cout << "\nElemen array:\n";
-  for (int i = 0; i < dimensi[0]; ++i) {
-    for (int j = 0; j < dimensi[1]; ++j) {
-      for (int k = 0; k < dimensi[2]; ++k) {
+  for (int i = 0; i < dimensi[0]; ++i) 
+  {
+    for (int j = 0; j < dimensi[1]; ++j) 
+    {
+      for (int k = 0; k < dimensi[2]; ++k) 
+      {
         cout << arr[i][j][k] << " ";
       }
       cout << endl;
@@ -43,13 +52,14 @@ int main() {
   }
 
   // Bebaskan memori untuk array 3 dimensi (kebalikan dari urutan alokasi)
-  for (int i = 0; i < dimensi[0]; ++i) {
-    for (int j = 0; j < dimensi[1]; ++j) {
+  for (int i = 0; i < dimensi[0]; ++i) 
+  {
+    for (int j = 0; j < dimensi[1]; ++j) 
+    {
       delete[] arr[i][j];
     }
     delete[] arr[i];
   }
   delete[] arr;
-
   return 0;
 }
