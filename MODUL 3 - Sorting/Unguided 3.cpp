843885f3cee4801f5ed510a8b5544976eb85520a
Darussalaam Nur Rasyidu <donnatamara820@gmail.com>
2024-03-31 13:26:52 +0700

    Update Unguided 3

--- MODUL 3 - Sorting/Unguided 3.cpp
+++ MODUL 3 - Sorting/Unguided 3.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(char arr[], int a) {
+void bubbleSort(char arr[], int a)
+{
   int i, j;
-  for (i = 0; i < a - 1; i++) {
-    for (j = 0; j < a - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+  for (i = 0; i < a - 1; i++)
+  {
+    for (j = 0; j < a - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
         char temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
@@ -14,7 +18,8 @@ void bubbleSort(char arr[], int a) {
   }
 }
 
-int main() {
+int main()
+{
   int a;
   cout << "input <n> = ";
   cin >> a;
@@ -22,14 +27,16 @@ int main() {
   char karakter[a];
 
   // Meminta user untuk menginputkan karakter
-  for (int i = 0; i < a; i++) {
+  for (int i = 0; i < a; i++)
+  {
     cout << "Karakter ke-" << i + 1 << ": ";
     cin >> karakter[i];
   }
 
   // Menampilkan karakter sebelum sorting
   cout << "\nUrutan karakter sebelum sorting:\n";
-  for (int i = 0; i < a; i++) {
+  for (int i = 0; i < a; i++)
+  {
     cout << karakter[i] << " ";
   }
 
@@ -38,12 +45,14 @@ int main() {
 
   // Menampilkan karakter setelah ascending sort
   cout << "\n\nUrutan karakter setelah ascending sort:\n";
-  for (int i = 0; i < a; i++) {
+  for (int i = 0; i < a; i++)
+  {
     cout << karakter[i] << " ";
   }
 
   // Sorting descending dengan bubble sort
-  for (int i = 0; i < a / 2; i++) {
+  for (int i = 0; i < a / 2; i++)
+  {
     char temp = karakter[i];
     karakter[i] = karakter[a - i - 1];
     karakter[a - i - 1] = temp;
@@ -51,7 +60,8 @@ int main() {
 
   // Menampilkan karakter setelah descending sort
   cout << "\n\nUrutan karakter setelah descending sort:\n";
-  for (int i = 0; i < a; i++) {
+  for (int i = 0; i < a; i++)
+  {
     cout << karakter[i] << " ";
   }
 
