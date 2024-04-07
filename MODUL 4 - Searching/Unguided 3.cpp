b4e74bef9d9fe56abd9ac3c04e43d6f24868fae0
Darussalaam Nur Rasyidu <donnatamara820@gmail.com>
2024-04-07 11:58:47 +0700

    Update Unguided 3

--- MODUL 4 - Searching/Unguided 3.cpp
+++ MODUL 4 - Searching/Unguided 3.cpp
@@ -2,28 +2,36 @@
 
 using namespace std;
 
-int sequentialSearch(int data[], int size, int target, int& index) {
+void sequentialSearch(const int data[], int size, int target, int indices[], int& count) {
+    count = 0; 
     for (int i = 0; i < size; i++) {
         if (data[i] == target) {
-            index = i;
-            return 1; // Kembalikan 1 jika angka ditemukan
+            indices[count] = i; // Simpan indeks jika angka ditemukan
+            count++; 
         }
     }
-    return 0; // Kembalikan 0 jika angka tidak ditemukan
 }
 
 int main() {
     int data[] = {9, 4, 1, 4, 7, 10, 5, 4, 12, 4};
     int size = sizeof(data) / sizeof(data[0]); // Menghitung panjang array secara dinamis
-    int target, index;
+    int target, count;
+    const int max = 100; // Ukuran maksimum untuk array indeks
+    int indices[max];
 
     cout << "Masukkan angka yang ingin Anda cari: ";
     cin >> target;
 
-    if (sequentialSearch(data, size, target, index)) {
-        cout << "Angka " << target << " ditemukan pada indeks ke-" << index << endl;
+    sequentialSearch(data, size, target, indices, count);
+
+    if (count > 0) {
+        cout << "Angka " << target << " ditemukan sebanyak " << count << " kali, pada indeks:" << endl;
+        for (int i = 0; i < count; i++) {
+            cout << indices[i] << " ";
+        }
+        cout << endl;
     } else {
-        cout << "Angka " << target << " tidak ditemukan dalam data" << endl;
+        cout << "Angka " << target << " tidak ditemukan dalam data." << endl;
     }
 
     return 0;
