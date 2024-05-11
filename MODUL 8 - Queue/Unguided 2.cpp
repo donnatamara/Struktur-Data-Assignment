808ee3dfb4b8629b995e22a2914d9d2f0f742cb1
Darussalaam Nur Rasyidu <donnatamara820@gmail.com>
2024-05-11 23:30:35 +0700

    Update Unguided 2

--- MODUL 8 - Queue/Unguided 2.cpp
+++ MODUL 8 - Queue/Unguided 2.cpp
@@ -2,72 +2,89 @@
 
 using namespace std;
 
-struct Mahasiswa {
+struct Mahasiswa
+{
     string nama;
     string nim;
-    Mahasiswa* next;
+    Mahasiswa *next;
 };
 
-Mahasiswa* front = NULL;
-Mahasiswa* back = NULL;
+Mahasiswa *front = NULL;
+Mahasiswa *back = NULL;
 
-bool isEmpty() {
+bool isEmpty()
+{
     return front == NULL;
 }
 
-void enqueueAntrian(string nama, string nim) {
-    Mahasiswa* newNode = new Mahasiswa;
+void enqueueAntrian(string nama, string nim)
+{
+    Mahasiswa *newNode = new Mahasiswa;
     newNode->nama = nama;
     newNode->nim = nim;
     newNode->next = NULL;
 
-    if (isEmpty()) {
+    if (isEmpty())
+    {
         front = newNode;
         back = newNode;
-    } else {
+    }
+    else
+    {
         back->next = newNode;
         back = newNode;
     }
 }
 
-void dequeueAntrian() {
-    if (isEmpty()) {
+void dequeueAntrian()
+{
+    if (isEmpty())
+    {
         cout << "Antrian kosong" << endl;
-    } else {
-        Mahasiswa* temp = front;
+    }
+    else
+    {
+        Mahasiswa *temp = front;
         front = front->next;
         delete temp;
     }
 }
 
-int countQueue() {
+int countQueue()
+{
     int count = 0;
-    Mahasiswa* current = front;
-    while (current != NULL) {
+    Mahasiswa *current = front;
+    while (current != NULL)
+    {
         count++;
         current = current->next;
     }
     return count;
 }
 
-void clearQueue() {
-    while (!isEmpty()) {
+void clearQueue()
+{
+    while (!isEmpty())
+    {
         dequeueAntrian();
     }
 }
 
-void viewQueue() {
+void viewQueue()
+{
     cout << "Data antrian mahasiswa: " << endl;
-    Mahasiswa* current = front;
+    Mahasiswa *current = front;
     int index = 1;
-    while (current != NULL) {
+    while (current != NULL)
+    {
         cout << index << ". Nama: " << current->nama << ", NIM: " << current->nim << endl;
         current = current->next;
         index++;
     }
 }
 
-int main() {
+int main()
+{
     enqueueAntrian("Donna", "2311110014");
     enqueueAntrian("Dundun", "2311110114");
     viewQueue();
